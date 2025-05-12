7714d8e91a93ec53f728622c59da8c7debb4b4a4
DilanFuentes <fdilan069@gmail.com>
2025-05-12 12:08:47 -0400

    Creacion proyecto perfulandia

---
+++ proyectoPSPA/src/main/java/com/perfulandia/proyectoPSPA/ProyectoPspaApplication.java
@@ -0,0 +1,15 @@
+package com.perfulandia.proyectoPSPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPspaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPspaApplication.class, args);
+
+		
+	}
+
+}
---
+++ proyectoPSPA/src/test/java/com/perfulandia/proyectoPSPA/ProyectoPspaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfulandia.proyectoPSPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPspaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
